1688175b44d493fafdc74837c4bd44cd7e8d2758
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-29 08:05:48 +0430

    sidebar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+// components
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import Main from "./components/Main";
 // context
 import SpotifyContextProvider from "./contexts/SpotifyContextProvider";
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+// icons
+import { BsSpotify, BsFillInfoCircleFill } from "react-icons/bs";
+import { SiApplemusic } from "react-icons/si";
+import { IoMdContact } from "react-icons/io";
+import { MdFavorite } from "react-icons/md";
 
 const Sidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <aside className="aside-container">
+      <div className="d-flex align-items-center p-4">
+        <BsSpotify
+          style={{ fontSize: "27px", color: "rgb(29 185 84)", cursor: "auto" }}
+        />
+        <h4 className="p-1">Spotify</h4>
+      </div>
+      <div>
+        <ul className="sidebar-list">
+          <a href="#">
+            <li className="d-flex align-items-center">
+              <SiApplemusic className="m-1" />
+              All Songs
+            </li>
+          </a>
+
+          <a href="#">
+            <li className="d-flex align-items-center">
+              <MdFavorite className="m-1" />
+              Favorites
+            </li>
+          </a>
+
+          <a href="#">
+            <li className="d-flex align-items-center">
+              <IoMdContact className="m-1" />
+              Contact us
+            </li>
+          </a>
+
+          <a href="#">
+            <li>
+              <BsFillInfoCircleFill className="m-1" />
+              About Us
+            </li>
+          </a>
+        </ul>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
