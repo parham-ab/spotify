d0ac597553ea76199505c902ec459673a977d916
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-30 11:42:42 +0430

    about us pge completed

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const AboutUs = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="d-flex flex-column justify-content-center m-auto p-3">
+      <div>
+        <h5>
+          Made with <span className="text-danger">❤</span> by{" "}
+          <a
+            href="https://parham-ab.netlify.app/"
+            target={"_blank"}
+            rel="noreferrer"
+            className="text-primary"
+          >
+            {" "}
+            Parham Abolghasemi
+          </a>
+        </h5>
+      </div>
+      <hr />
+      <div>
+        <p>
+          Copyright <sup>©</sup> Parham Abolghasemi 2022 All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
